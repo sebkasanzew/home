e639fa35d6d38337b3cfeba5810ca052aac5842d
Sebastian Kasanzew <sebkasanzew@gmail.com>
2024-12-02 22:54:22 +0100

    Add image for day 3

--- src/app/advent/2024/3/page.tsx
+++ src/app/advent/2024/3/page.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 export const metadata = {
   title: "3. Dezember",
 };
 
 export default function Day3() {
-  return null;
+  return (
+    <Image
+      src="https://utfs.io/f/DtDWmEQ5jde7PVA2mYpnbkvNj2WJu9SQMT6rglsid4BwoeEI"
+      alt="Daft Punk as Santa"
+      width={1024}
+      height={1024}
+    />
+  );
 }
