48306ef02712b0bdb9b3852eb7b85e830d08902a
Sebastian Kasanzew <sebkasanzew@gmail.com>
2024-12-06 23:06:08 +0100

    Add day 7

--- src/app/advent/2024/7/page.tsx
+++ src/app/advent/2024/7/page.tsx
@@ -3,5 +3,10 @@ export const metadata = {
 };
 
 export default function Day7() {
-  return null;
+  return (
+    <p>
+      {`Zu viel "7 vs. Wild" Staffel 4 geguckt heute. Und auch richtig viel Lego
+      gebaut. Jetzt ist schon 23 Uhr, kein Bock.`}
+    </p>
+  );
 }
